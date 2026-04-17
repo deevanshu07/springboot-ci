3e9f1cfee5996d5a894136522e73316e045a7a48
Deevanshu Mahajan <deevanshumahajan07@gmail.com>
2026-04-17 12:06:53 +0000

    Testing the pipeline

--- src/main/java/com/example/springbootci/SpringbootCiApplication.java
+++ src/main/java/com/example/springbootci/SpringbootCiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootCiApplication {
 
-    public static int add(int a, int b) {
-        return a + b;
+    public static int add(int a, int c) {
+        return a + c;
     }
     public static void main(String[] args) {
         SpringApplication.run(SpringbootCiApplication.class, args);
